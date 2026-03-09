0b7642ac31205bdba042bca076e54da16ac61644
Julia Silva de Souza <juliasilvadesouza09@gmail.com>
2026-03-09 20:53:47 -0300

    Projeto Final?

--- src/App.java
+++ src/App.java
@@ -10,34 +10,39 @@ public class App {
     public static void main(String[] args) throws Exception {
         boolean rodando = true;
             while(rodando){ 
-                System.out.println("==========================\n\n= Bem-vindo - Biblioteca =\n\n==========================");
-                System.out.println("========== MENU ==========\n");
-                System.out.println("= 1 - Listar Acervo\n"+
-                                    "= 2 - Listar Usuários\n"+
-                                    "= 3 - Emprestar Livro\n"+
-                                    "= 4 - Devolver Livro\n"+
-                                    "= 5 - Cadastrar Livro\n"+
-                                    "= 5 - Cadastrar Livro\n"+
-                                    "= 6 - Cadastrar Usuario\n"+
-                                    "= 0 - Sair");
-
-                System.out.println("Escolha uma opção: ");
+                System.out.println("\n╔═════════════════════════╗");
+                System.out.println("║ Bem-Vindos a Biblioteca ║");
+                System.out.println("╟─────────────────────────╢");
+                System.out.println("║ ──────── MENU ───────── ║");
+                System.out.println("║ = 1 - Listar Acervo     ║\n"+
+                                    "║ = 2 - Listar Usuários   ║\n"+
+                                    "║ = 3 - Emprestar Livro   ║\n"+
+                                    "║ = 4 - Devolver Livro    ║\n"+
+                                    "║ = 5 - Cadastrar Livro   ║\n"+
+                                    "║ = 6 - Cadastrar Usuario ║\n"+
+                                    "║ = 0 - Sair              ║");
+                System.out.println("╚═════════════════════════╝\n");
+                System.out.println("  = Escolha uma opção:     ");
                 String opcao = scanner.nextLine().trim();
 
                 switch (opcao) {
                     case "1":
-                        System.out.println("\n\n= LIVROS CADASTRADOS: =");
+                        System.out.println("\n╔═════════════════════════╗");
+                        System.out.println("║   LIVROS CADASTRADOS:   ║");
+                        System.out.println("╚═════════════════════════╝\n");
                         acervo.forEach(System.out::println);
                         break;
                     case "2":
-                        System.out.println("\n\n= USUARIOS CADASTRADOS: =");
+                        System.out.println("\n╔═════════════════════════╗");
+                        System.out.println("║  USUARIOS CADASTRADOS:  ║");
+                        System.out.println("╚═════════════════════════╝\n");
                         usuarios.forEach(System.out::println);
                         break;
                     case "3":
-                        //pegar livros emprestado
+                        emprestarLivro();
                         break;
                     case "4":
-                        //devolver livros
+                        devolverLivro();
                         break;
                     case "5":
                         cadastrarLivros();
@@ -57,43 +62,85 @@ public class App {
         }
 
 public static void cadastrarUsuario(){
-        System.out.println("==========================");
-        System.out.println("=    CADASTRAR USUARIO   =");
+    System.out.println("\n╔═════════════════════════╗");
+    System.out.println("║    CADASTRAR USUARIO    ║");
+    System.out.println("╚═════════════════════════╝\n");
 
-        System.out.println("Nome de Usuario: ");
-        String nome = scanner.nextLine().trim();
+    System.out.println("Nome de Usuario: ");
+    String nome = scanner.nextLine().trim();
 
-        System.out.println("CPF: ");
-        String cpf = scanner.nextLine().trim();
+    System.out.println("CPF: ");
+    String cpf = scanner.nextLine().trim();
 
-        System.out.println("E-mail: ");
-        String email = scanner.nextLine().trim();
+    System.out.println("E-mail: ");
+    String email = scanner.nextLine().trim();
 
-        System.out.println("Usuario Cadastrado!");
-        Usuario novoUsuario = new Usuario(nome, cpf, email);
+    System.out.println("Usuario Cadastrado!");
+    Usuario novoUsuario = new Usuario(nome, cpf, email);
         
-        usuarios.add(novoUsuario);
+    usuarios.add(novoUsuario);
+    }
+
+public static void devolverLivro(){
+
+    System.out.println("\n╔═════════════════════════╗");
+    System.out.println("║      DEVOLVER LIVRO     ║");
+    System.out.println("╚═════════════════════════╝\n");
+
+    System.out.println("Digite o código do livro:");
+    String codigo = scanner.nextLine();
+
+    for(Livro livro : acervo){
+        if(livro.getCode().equals(codigo)){
+            if(!livro.foiEmprestado()){
+                System.out.println("Esse livro já está disponível!");
+            }else{
+                livro.devolver();
+                System.out.println("Livro devolvido com sucesso!");
+            } return;
         }
+    }
 
-public static void cadastrarLivros(){
-        System.out.println("==========================");
-        System.out.println("=    CADASTRAR LIVRO  =");
-
-        System.out.println("Titulo do Livro: ");
-        String Titulo = scanner.nextLine().trim();
-
-        System.out.println("Autor: ");
-        String Autor = scanner.nextLine().trim();
-
-        System.out.println("Livro Cadastrado!");
-        Livro novoLivro = new Livro(Titulo, Autor);
-
-        acervo.add(novoLivro);
-        }
+    System.out.println("Livro não encontrado.");
 }
 
-/*public String gerarCodigo() {
-    Random random = new Random();
-    int numero = random.nextInt(900000) + 100000;
-    return String.valueOf(numero);
-}*/
+public static void emprestarLivro(){
+
+    System.out.println("\n╔═════════════════════════╗");
+    System.out.println("║    EMPRESTAR LIVROS     ║");
+    System.out.println("╚═════════════════════════╝\n");
+
+    System.out.println("Digite o código do livro:");
+    String codigo = scanner.nextLine();
+
+    for(Livro livro : acervo){
+        if(livro.getCode().equals(codigo)){
+            if(livro.foiEmprestado()){
+                System.out.println("Livro já está emprestado!");
+            }else{
+                livro.emprestar();
+                System.out.println("Livro emprestado com sucesso!");
+            } return;
+        }
+    }
+
+    System.out.println("Livro não encontrado.");
+}
+
+public static void cadastrarLivros(){
+    System.out.println("\n╔═════════════════════════╗");
+    System.out.println("║     CADASTRAR LIVRO     ║");
+    System.out.println("╚═════════════════════════╝\n");
+
+    System.out.println("Titulo do Livro: ");
+    String Titulo = scanner.nextLine().trim();
+
+    System.out.println("Autor: ");
+    String Autor = scanner.nextLine().trim();
+
+    System.out.println("Livro Cadastrado!");
+    Livro novoLivro = new Livro(Titulo, Autor);
+
+    acervo.add(novoLivro);
+    }
+}
--- src/Livro.java
+++ src/Livro.java
@@ -1,16 +1,42 @@
+import java.util.Random;
+
 public class Livro {
     private String code;
     private String nome;
     private String autor;
-    private boolean status;
+    private boolean emprestado;
 
-    //add o codigo e os status dps
     public Livro(String nome, String autor){
         this.nome = nome;
         this.autor = autor;
-        this.status = status;
+        this.code = gerarCodigo();
+        this.emprestado = false;
     }
+
+    private String gerarCodigo() {
+        Random random = new Random();
+        int numero = random.nextInt(900000) + 100000;
+        return String.valueOf(numero);
+    }
+
+    public String getCode(){
+        return code;
+    }
+
+    public boolean foiEmprestado(){
+        return emprestado;
+    }
+
+    public void emprestar(){
+        emprestado = true;
+    }
+
+    public void devolver(){
+        emprestado = false;
+    }
+
+
     public String toString() {
-        return "Titulo: " + this.nome + " | Autor: " + this.autor + " | Código: " + this.code; 
+        return "Titulo: " + this.nome + " | Autor: " + this.autor + " | Código: " + this.code + " | emprestado: "+ this.emprestado; 
     }
 }
