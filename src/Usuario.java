222f921af45717d3eb1091a4815c157bc3ccf49b
wagnermarques <wagnerdocri@gmail.com>
2026-03-10 09:10:09 -0300

    Projeto Final? V2

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,9 +1,10 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Usuario {
     Scanner scanner = new Scanner(System.in);
     private String nome;
-    private String code;
+    private String ID;
     private String cpf;
     private String email;
 
@@ -11,10 +12,16 @@ public class Usuario {
     this.nome = nome;
     this.cpf = cpf;
     this.email = email;
+    this.ID = gerarID();
     }
 
-    public String toString() {
-    return "Usuário: " + this.nome + " | CPF: " + this.cpf + " | E-mail: " + this.email; 
-}
+    private String gerarID() {
+      Random random = new Random();
+      int numero = random.nextInt(900000) + 100000;
+      return String.valueOf(numero);
+   }
 
+    public String toString() {
+    return "Usuário: " + this.nome + " | CPF: " + this.cpf + " | E-mail: " + this.email + " | ID: " + this.ID;  
+    }
 }
